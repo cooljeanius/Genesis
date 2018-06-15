011ee554fa731d8e64fee039e0063805ab92115c
nemaara <nemaara@gmail.com>
2018-06-15 04:42:13 -0400

    Building framework for spell casting system

--- lua/spells.lua
+++ lua/spells.lua
@@ -1,15 +1,77 @@
-
----
--- Turns a unit X times
--- Only affects the first unit matching the filter
--- [multi_turn_unit]
---     [filter]
---         ... SUF ...
---     [/filter]
---     times={X}
--- [/multi_turn_unit]
----
 yumi_spell_radius=0
+
+yumi_spells = {"Ethereal Blast","Void Blast", "Cancel"}
+yumi_spell_images = {"attacks/faerie-fire.png","attacks/dark-missile.png","attacks/blank-attack.png"}
+yumi_spell_radii = {1,1,1}
+
+function wesnoth.wml_actions.void_blast_spell()
+    wesnoth.play_sound("shaxthal-energy-prelude.ogg")
+    wesnoth.delay(100)
+    wesnoth.play_sound("shaxthal-energy-fire.ogg")
+    wesnoth.wml_actions.harm_unit {fire_event="yes",
+        animate="yes",amount="10",delay="50",experience="yes",
+        {"filter",{x="$x1",y="$y1"}},
+        {"filter_second",{id="Yumi"}}
+    }
+end
+
+function wesnoth.wml_actions.yumi_spell_menu()
+
+    local helper = wesnoth.require "lua/helper.lua"
+    local T = helper.set_wml_tag_metatable {}
+
+    local dialog = {
+      T.tooltip { id = "tooltip_large" },
+      T.helptip { id = "tooltip_large" },
+      T.grid { T.row {
+        T.column { T.grid {
+          T.row { T.column { horizontal_grow = true, T.listbox { id = "list",
+            T.list_definition { T.row { T.column { horizontal_grow = true,
+              T.toggle_panel { return_value = -1, T.grid { T.row {
+                T.column { horizontal_alignment = "left", T.label { id = "label" } },
+                T.column { T.image { id = "icon" } }
+              } } }
+            } } }
+          } } },
+          T.row { T.column { T.grid { T.row {
+            T.column { T.button { id = "ok", label = "OK" } },
+            T.column { T.button { id = "cancel", label = "Cancel" } }
+          } } } }
+        } },
+        T.column { T.image { id = "image" } }
+      } }
+    }
+    
+    local function preshow()
+        local function select()
+            local i = wesnoth.get_dialog_value "list"
+        end
+        wesnoth.set_dialog_callback(select, "list")
+        for i,v in ipairs(yumi_spells) do
+            wesnoth.set_dialog_value(v, "list", i, "label")
+        end
+        for i,v in ipairs(yumi_spell_images) do
+            wesnoth.set_dialog_value(v, "list", i, "icon")
+        end
+        wesnoth.set_dialog_value(2, "list")
+        select()
+    end
+
+    local li
+    local function postshow()
+        li = wesnoth.get_dialog_value "list"
+    end
+
+    local r = wesnoth.show_dialog(dialog, preshow, postshow)
+    
+    if li==1 then
+        wesnoth.wml_actions.void_blast_spell()
+    elseif li==2 then
+        wesnoth.wml_actions.void_blast_spell()
+    end
+    
+    --wesnoth.message(string.format("Button %d pressed. Item %d selected.", r, li))
+end
 
 function wesnoth.wml_actions.refresh_spell_menu_yumi(cfg)
     local yumi = wesnoth.get_unit("Yumi")
@@ -17,35 +79,21 @@ function wesnoth.wml_actions.refresh_spell_menu_yumi(cfg)
     wesnoth.wml_actions.set_menu_item { id="yumi_spells",
         description="Cast Spell (Yumi)",
         {"filter_location",{
-            {"filter_adjacent_location", {radius=yumi_spell_radius,
-                {"filter",{id="Yumi"}}}
+            {"filter",{id="Yumi"}},
+            {"or",{
+                {"filter_adjacent_location", {radius=yumi_spell_radius,
+                    {"filter",{id="Yumi"}}
+                }}
             }}
-        },
+        }},
         {"command",{
-            {"message",{speaker="narrator",
-                {"option",{message="Void Blast",image="attacks/dark-missile.png",
-                    {"command",{{"set_variable",{
-                        name="yumi_spell_index",
-                        value="1"
-                    }}}}
-                }} 
-            }}
+            {"yumi_spell_menu"}
         }}
     }
-    --local spell_ind = wesnoth.get_variable("yumi_spell_index")
-    --wesnoth.wml_actions.clear_variable {name = "yumi_spell_index"}
-    local spell_ind = 1
-    if spell_ind==1 then
-        wesnoth.wml_actions.harm_unit {fire_event="yes",
-            animate="yes",amount="10",delay="50",kill="no",
-            {"filter",{x="$x1",y="$y1"}}
-        }
-    elseif spell_ind==2 then
-        spell_ind=2
-    end
 end
 
-function wesnoth.wml_actions.add_spell_yumi(cfg)
-    yumi_spell_radius=10
-
+function wesnoth.wml_actions.add_void_blast_yumi(cfg)
+    yumi_spell_radius=5
+    void_blast=1
+    wesnoth.wml_actions.refresh_spell_menu_yumi(cfg)
 end
