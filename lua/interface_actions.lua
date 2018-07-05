1596143dc576547e20073357e017206d6ec10a4b
nemaara <nemaara@gmail.com>
2018-07-05 02:17:12 -0400

    Added credits_text to lua

--- lua/interface_actions.lua
+++ lua/interface_actions.lua
@@ -49,3 +49,63 @@ function wesnoth.wml_actions.multi_turn_unit(cfg)
     end
 
 end
+
+local wml_actions = wesnoth.wml_actions
+
+function alpha_print(text, size, alpha)
+
+    local c = helper.round(255 * alpha)
+
+    wml_actions.print({
+        text = text,
+        size = size,
+        red = c, green = c, blue = c,
+        duration=1000
+    })
+
+    wesnoth.delay(20)
+
+    wml_actions.redraw({})
+end
+
+-- Arguments:
+--   text:          Text displayed
+--   duration:      Duration of the text after fade-in and before fade-out animations, in milliseconds
+function wml_actions.credits_text(cfg)
+
+    local title = cfg.title
+    local text = cfg.body
+    local duration = cfg.duration
+    --local fade_duration = cfg.fade_duration
+
+    if text == nil then
+        text = ""
+    end
+
+    if title ~= nil then
+        text = "<span size='larger' weight='bold'>" .. title .. "</span>\n\n" .. text;
+    end
+
+    if duration == nil then
+        duration = 5000
+    end
+
+    for alpha = 0.0, 1.0, 0.1 do
+        alpha_print(text, 20, alpha)
+    end
+
+    wesnoth.delay(duration)
+
+    for alpha = 1.0, 0.0, -0.1 do
+        alpha_print(text, 20, alpha)
+    end
+
+    wml_actions.print({
+        text = text,
+        size = 20,
+        red = 0, green = 0, blue = 0,
+        duration=10
+    })
+
+    wesnoth.delay(750)
+end
