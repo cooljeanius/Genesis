fa7e89b2684579ca1c3854d6a17749a547f4eb42
nemaara <nemaara@gmail.com>
2019-10-11 03:35:28 -0400

    E2S3a: complete scenario functionality

--- episode2/lua/scenarios/03a_Mirror_Lake.lua
+++ episode2/lua/scenarios/03a_Mirror_Lake.lua
@@ -86,7 +86,7 @@ function wesnoth.wml_actions.fire_puzzle_hex(cfg)
         wesnoth.set_variable("lake.fire_puzzle",6)
     elseif wesnoth.get_variable("lake.fire_puzzle") == 6 then
         wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>A shining star in the darkness, the keeper of the sacred flame needed to spark all Life... that is Shardia. A staunch protector against the savage beast hordes that hail from the depths of Norsula's oceans. That... is Shardia.</i>")}
-        wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>But you know as well as I do, don't you? The fire that burns the brightest is also the one that consumes the most. Yours is but a facade of warmth. In truth, you embrace carnage as much as I do, so long as it suits your means. Don't you?</i>")}
+        wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>But you know as well as I do, don't you? The fire that burns the brightest is also the one that consumes the greatest. Yours is but a facade of warmth. In truth, you embrace carnage as much as I, so long as it suits your means. We are not so different. You know this, no matter how much you protest with your feeble words.</i>")}
         wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>Flame both is the spark of life and that which burns it to ashen cinders. Isn't that right, Shardia?</i>")}
     end
 end
@@ -136,10 +136,10 @@ function wesnoth.wml_actions.ice_puzzle_hex(cfg)
         
         wesnoth.set_variable("lake.ice_puzzle",4)
     elseif wesnoth.get_variable("lake.ice_puzzle") == 4 then
-        wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>Water and fire together, like the push and pull of the mighty tide. The deep current and the searing flame - we are mortal enemies persisting on one plane, locked in a dance of eternal combat.</i>")}
-        wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>Relish the terror, relish the blood! Don thy macabre masque and lock thy arms with mine! The steel that drives our bellicose dance demands the presence of both; water and fire, without one, the other is lost.</i>")}
-        wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>Opposed, but kindred spirits, we bear that hatred which is a second instinct to necessity. It is an antipathy that binds us, whether you like it or not.</i>")}
-        wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>Shardia...</i>")}
+        wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>Frozen water, mirror ice, that which may be brittle enough to shatter with a mere breath, or that which can withstand all but the mightiest of strikes. It is uncanny, is it not? Given the right density, water... merely frozen water, can be as strong as the steel forged in the depths of our great furnaces on Norsula.</i>")}
+        wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>A marvelous substance. A pity its Guardian only uses it for meaningless mystic nonsense. If Yukiria put the same effort into harnessing the physical and magical properties of her element for battle, she could raise an army to rival even mine.</i>")}
+        wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>Imagine. Ice, a material, when used properly, that can reflect all manner of magic and whose cutting power only grows as it shatters further. A freezing cold that can pierce through the hardest metal and a magic mirror which can reflect and distort reality itself. Combined with the physical imperveability of water, I would be unstoppable.</i>")}
+        wesnoth.wml_actions.message {speaker="narrator", message=string.format("<i>She cares not for the battle in which we partake, but perhaps Yukiria can still be convinced. We shall see.</i>")}
     end
 end
 function wesnoth.wml_actions.ice_puzzle_grid_hex(cfg)
