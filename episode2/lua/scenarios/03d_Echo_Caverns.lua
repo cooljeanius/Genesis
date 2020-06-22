8e9958a54899bb1f70e64395e3e6bcdc7918b990
nemaara <nemaara@gmail.com>
2020-06-22 04:36:02 -0400

    Fix some code in S3d

--- episode2/lua/scenarios/03d_Echo_Caverns.lua
+++ episode2/lua/scenarios/03d_Echo_Caverns.lua
@@ -6,43 +6,16 @@ function wesnoth.wml_actions.init_puzzles(cfg)
     wesnoth.set_variable("echo.nightmare",0)
 end
 
-function wesnoth.wml_actions.fishing_puzzle_hex(cfg)
+function wesnoth.wml_actions.firebolt_mirror(cfg)
     local u = wesnoth.get_unit("Esther")
-    if wesnoth.get_variable("ethea.fishing") == 1 and u.x == 55 and u.y ==32 then
-        wesnoth.wml_actions.message {speaker="Esther", message="Hmm. I suppose I could melt the ice for that crabby old dog."}
-        wesnoth.wml_actions.message {speaker="Aryel", message="If you want."}
-        wesnoth.wml_actions.message {speaker="Esther", message="Well, why not?"}
-        wesnoth.delay(500)
-        wesnoth.play_sound("fire.wav")
-        wesnoth.set_terrain(55,32,"Wwt")
-        wesnoth.fire("redraw")
+    local t = wesnoth.get_terrain(14,11)
+    local x1 = wesnoth.get_variable("spell_target.x")
+    local y1 = wesnoth.get_variable("spell_target.y")
+    if wesnoth.get_variable("echo.mirror") == 1 and t == "Wwby" and x1==14 and y1==11 then
+        --wesnoth.play_sound("fire.wav")
+        --wesnoth.set_terrain(55,32,"Wwt")
+        --wesnoth.fire("redraw")
         wesnoth.wml_actions.message {speaker="Esther", message="All done."}
-        wesnoth.set_variable("ethea.fishing",2)
-    elseif wesnoth.get_variable("ethea.fishing") == 1 then
-        wesnoth.wml_actions.message {speaker="Aryel", message="Guess we need Esther for this."}
-    elseif wesnoth.get_variable("ethea.fishing") == 3 then
-        wesnoth.wml_actions.message {speaker="Aryel", message="Should be easy to catch some fish, if you gals are hungry."}
-        wesnoth.wml_actions.message {speaker="Esther", message="Not me. Yumi?"}
-        wesnoth.wml_actions.message {speaker="Yumi", message="It stinks..."}
-        wesnoth.wml_actions.message {speaker="Aryel", message="Alright. All for me, then."}
-        wesnoth.wml_actions.add_hp {hp=1,{"filter",{id="Aryel"}}}
-        wesnoth.set_variable("ethea.fishing",4)
-    end
-end
-
-function wesnoth.wml_actions.temple_puzzle_hex(cfg)
-    local t = wesnoth.get_terrain(45,9)
-    if t == "Bryd^Ii" and wesnoth.get_variable("ethea.temple") == 0 then
-        wesnoth.delay(200)
-        wesnoth.play_sound("rumble.ogg")
-        wesnoth.play_sound("rumble.ogg")
-        wesnoth.delay(500)
-        wesnoth.set_terrain(48,5,"Dd")
-        wesnoth.set_terrain(48,6,"Drt")
-        wesnoth.fire("redraw")
-        wesnoth.delay(500)
-        wesnoth.wml_actions.message {speaker="Esther", message="Whoa. Those mountains just... disappeared. Looks like there's some kind of temple inside?"}
-        wesnoth.wml_actions.message {speaker="Aryel", message="Might be worth checking out."}
-        wesnoth.set_variable("ethea.temple",1)
+        wesnoth.set_variable("echo.mirror",2)
     end
 end
